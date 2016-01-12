94a31d372ebb5a97c7daf4b88434924ade093ccf
yjwfn <head_main@qq.com>
2016-01-12 18:14:31 +0800

    Implement RulerView

--- app/src/main/java/com/lw/MainActivity.java
+++ app/src/main/java/com/lw/MainActivity.java
@@ -5,11 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.lw.widget.RulerView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,74 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+
+        init();
+    }
+
+    private void init(){
+
+        final RulerView rulerView = (RulerView) findViewById(R.id.ruler_view);
+        final EditText positionEditText = (EditText) findViewById(R.id.et_pos);
+        final EditText valueEditText = (EditText) findViewById(R.id.et_value);
+        Button  scrollPosition = (Button) findViewById(R.id.btn_scroll_position);
+        Button  scrollVal = (Button) findViewById(R.id.btn_scroll_value);
+        Button topBtn = (Button) findViewById(R.id.btn_top);
+        Button bottomBtn = (Button) findViewById(R.id.btn_bottom);
+
+
+        //init
+        rulerView.post(new Runnable() {
+            @Override
+            public void run() {
+                int position = Integer.valueOf(positionEditText.getText().toString());
+                rulerView.smoothScrollTo(position);
+            }
+        });
+
+        //scroll to position
+        scrollPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.valueOf(positionEditText.getText().toString());
+                rulerView.smoothScrollTo(position);
+            }
+        });
+
+        //scroll to value
+        scrollVal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int value = Integer.valueOf(valueEditText.getText().toString());
+                rulerView.smoothScrollToValue(value);
+            }
+        });
+
+
+        topBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rulerView.setGravity(Gravity.TOP);
+            }
+        });
+
+        bottomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rulerView.setGravity(Gravity.BOTTOM);
+            }
+        });
+
+
+        rulerView.setOnScaleListener(new RulerView.OnScaleListener() {
+            @Override
+            public void onScaleChanged(int scale) {
+                valueEditText.setText(scale + "");
+                positionEditText.setText((scale ) + "");
+            }
+        });
+
     }
 
     @Override
